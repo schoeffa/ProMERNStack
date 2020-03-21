2c600f9a770c6dc5418b75c6a8685c12193fd804
Aaron Schoeff <schoeffa@gmail.com>
2020-03-21 13:50:57 -0400

    Began building API using Express and GraphQL

--- server/server.js
+++ server/server.js
@@ -1,7 +1,58 @@
+const fs = require('fs');
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+let aboutMessage = "Issue Tracker API v1.0";
+
+const issuesDB = [
+    {
+        id: 1,
+        status: 'New',
+        owner: 'Ravan',
+        effort: 5,
+        created: new Date('2018-08-15'),
+        due: undefined,
+        title: 'Error in console when clicking Add'
+    }, 
+    {
+        id: 2,
+        status: 'Assigned',
+        owner: 'Eddie',
+        effort: 14,
+        created: new Date('2018-08-16'),
+        due: new Date('2018-08-30'),
+        title: 'Missing bottom border on panel'
+    }
+]
+
+const resolvers = {
+    Query: {
+        about: () => aboutMessage,
+        issueList,
+    },
+    Mutation: {
+        setAboutMessage,
+    },
+};
+
+function setAboutMessage(_, { message }) {
+    return aboutMessage = message;
+}
+
+function issueList() {
+    return issuesDB;
+}
+
+const server = new ApolloServer ({
+    typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
+    resolvers,
+});
+
 const app = express();
 
 app.use(express.static('public'));
+
+server.applyMiddleware({ app, path: '/graphql' });
 
 app.listen(3000, function() {
     console.log('App started on port 3000');
